901e07cc95c880ba719867eb608bf3cc4b7ad536
v <47469365+YosEdie@users.noreply.github.com>
2023-06-16 13:18:09 +0700

    exit

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -12,12 +12,14 @@ public class Menu extends PApplet {
     // Asset bg
     PImage backgroundImg;
     PImage playButtonImg;
+    PImage exitButtonImg; // New button image
     // Button variables
     int buttonX;
     int buttonY;
     int buttonWidth;
     int buttonHeight;
     boolean buttonPressed;
+    boolean exitButtonPressed; // New button state
 
     public void settings(){
         size(WIDTH, HEIGHT);
@@ -26,12 +28,14 @@ public class Menu extends PApplet {
     public void setup(){
         backgroundImg = loadImage("src/assets/background/intro.png");
         playButtonImg = loadImage("src/assets/sb.png");
+        exitButtonImg = loadImage("src/assets/e.png"); // Load the exit button image
         buttonWidth = 200;
         buttonHeight = 50;
         buttonX = (WIDTH - buttonWidth) / 2;
         buttonY = (HEIGHT - buttonHeight) / 2+100;
 
         buttonPressed = false;
+        exitButtonPressed = false; // Initialize the button state
 
         bgm = new BGMPlayer(music);
         bgm.BGMPlayer();
@@ -48,6 +52,14 @@ public class Menu extends PApplet {
             noTint();
         }
         image(playButtonImg, buttonX, buttonY, buttonWidth, buttonHeight);
+        // Draw the Exit button
+        if (exitButtonPressed) {
+            tint(255, 0, 0);
+        } else {
+            noTint();
+        }
+        image(exitButtonImg, buttonX, buttonY + buttonHeight + 20, buttonWidth, buttonHeight);
+
     }
 
     public void mousePressed() {
@@ -61,11 +73,19 @@ public class Menu extends PApplet {
                 surface.setVisible(false);
                 this.bgm.stop();
             }
+        }// Check if the mouse is clicked within the Exit button area
+        if (mouseX >= buttonX && mouseX <= buttonX + buttonWidth && mouseY >= buttonY + buttonHeight + 20 && mouseY <= buttonY + 2 * buttonHeight + 20) {
+            exitButtonPressed = true;
+            // Perform exit action here
+            if (exitButtonPressed) {
+                System.exit(0); // Terminate the program
+            }
         }
     }
 
     public void mouseReleased() {
         buttonPressed = false;
+        exitButtonPressed = false;
     }
 
 }
